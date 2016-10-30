ec07a6dcd4b834455cdef74d07a37ffec42ff8cb
KeithBeatTheBeast <keithgmills34@gmail.com>
2016-10-30 14:48:57 -0600

    Login Activity

---
+++ app/src/main/java/com/cmput301fa16t5/legendary_telegram/LogIn.java
@@ -0,0 +1,28 @@
+package com.cmput301fa16t5.legendary_telegram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Right now, this only contains the basic information.
+ * For simplicity, I've taken the liberty of instantiating all the view variables.
+ * I will do the same for the remaining activities.
+ */
+public class LogIn extends AppCompatActivity {
+
+    EditText userName;
+    Button login;
+    Button newUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        userName = (EditText) findViewById(R.id.userNameET);
+        login = (Button) findViewById(R.id.loginButton);
+        newUser = (Button) findViewById(R.id.newUserLogin);
+    }
+}
