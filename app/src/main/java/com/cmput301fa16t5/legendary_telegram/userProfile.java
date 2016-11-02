f9765c8861c9228424d1c61891093a69036ea1d3
KeithBeatTheBeast <keithgmills34@gmail.com>
2016-11-01 19:23:18 -0600

    Commented user profile java

--- app/src/main/java/com/cmput301fa16t5/legendary_telegram/userProfile.java
+++ app/src/main/java/com/cmput301fa16t5/legendary_telegram/userProfile.java
@@ -5,6 +5,26 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+/**
+This is the activity called upon when:
+1) A new user account is to be created (reached by login activity).
+2) An existing user account to be modified (reached by main request activity)
+
+In the first case, the fields are to be blank, filled with nothing, and the button at the bottom (changeSettings)
+says "create new account"
+
+In the second case, the fields are to be filled with the existing information about the user, which they can modify.
+The button at the bottom will say "change settings"
+
+I need to get rid of the password fields. Forgot about that. There is no password. Will do at home.
+
+The check button at the top checks the name entered into the field. If a user with that name already exists
+a new user account cannot be made/the existing account cannot be modified. 
+
+When the user creates or modifies an account the function that checks the name should be called anyway. THe 
+separate button exists for convience.
+*/
+
 public class userProfile extends AppCompatActivity {
 
     private Button checkUser;
