f2bb422a27651e0f97079b3f7813431c34c905fa
Schneeraubtier <snowraptor@live.ca>
2016-11-04 13:29:20 -0600

    Untested Elasticsearch and Request
    Copied the Elasticsearch code from the lab and fleshed out Request
    class, I think I have all the key variables covered with getters and
    setters.

--- app/src/main/java/com/cmput301fa16t5/legendary_telegram/ElasticSearchController.java
+++ app/src/main/java/com/cmput301fa16t5/legendary_telegram/ElasticSearchController.java
@@ -1,14 +1,113 @@
 package com.cmput301fa16t5.legendary_telegram;
 
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+import com.searchly.jestdroid.JestDroidClient;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+import com.searchly.jestdroid.JestDroidClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.core.DocumentResult;
+import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+
 /**
  * Created by keith on 11/2/2016.
  * It's the ESearch Controller
  * Gets, Posts, etc to ESearch.
  * Only accessed by the instance of CentralController.
  * Probably where we'll spend a lot of time.
+ *
+ * Code adapted from
+ * https://github.com/SRomansky/lonelyTwitter/blob/lab7end/app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
+ * from user SRomansky
  */
 
 public class ElasticSearchController {
+    private static JestDroidClient client;
 
-    private String serverName; // Where we put our stuff on Hindle's computer
+    // TODO we need a function that gets requests!
+    public static class GetRequestsTask extends AsyncTask<String, Void, ArrayList<Request>> {
+        @Override
+        protected ArrayList<Request> doInBackground(String... search_parameters) {
+            verifySettings();
+
+            ArrayList<Request> requests = new ArrayList<Request>();
+
+            // assume that search_parameters[0] is the only search term we are interested in using
+            Search search = new Search.Builder(search_parameters[0])
+                    .addIndex("fa16t5")
+                    .addType("request")
+                    .build();
+
+            try {
+                SearchResult result = client.execute(search);
+                if (result.isSucceeded()) {
+                    List<Request> foundRequests = result.getSourceAsObjectList(Request.class);
+                    requests.addAll(foundRequests);
+                }
+                else {
+                    Log.i("Error", "The search query failed to find any requests that matched.");
+                }
+            }
+            catch (Exception e) {
+                Log.i("Error", "Something went wrong when we tried to communicate with the elasticsearch server!");
+            }
+
+            return requests;
+        }
+    }
+
+
+    // TODO we need a function which adds a request!
+    public static class AddRequestsTask extends AsyncTask<Request, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Request... requests) {
+            verifySettings();
+
+            for (Request request: requests) {
+                Index index = new Index.Builder(request).index("testing").type("request").build();
+
+                try {
+                    DocumentResult result = client.execute(index);
+                    if (result.isSucceeded()) {
+                        request.setId(result.getId());
+                    }
+                    else {
+                        Log.i("Error", "Elastic search was not able to add the request.");
+                    }
+                }
+                catch (Exception e) {
+                    Log.i("Uhoh", "We failed to add a request to elastic search!");
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+    }
+
+
+    private static void verifySettings() {
+        // if the client hasn't been initialized then we should make it!
+        if (client == null) {
+            DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
+            DroidClientConfig config = builder.build();
+
+            JestClientFactory factory = new JestClientFactory();
+            factory.setDroidClientConfig(config);
+            client = (JestDroidClient) factory.getObject();
+        }
+    }
+
 }
--- app/src/main/java/com/cmput301fa16t5/legendary_telegram/Request.java
+++ app/src/main/java/com/cmput301fa16t5/legendary_telegram/Request.java
@@ -1,5 +1,7 @@
 package com.cmput301fa16t5.legendary_telegram;
 
+import android.location.Location;
+
 /**
  * Created by keith on 10/30/2016.
  * Request
@@ -12,6 +14,86 @@ public class Request {
 
     // Should have a field noting the ID that ElasticSearch gives it.
 
-    // Denotes the state of the request. See RequestEnum.java for details.
+    //ID created by ElasticSearchController
+    private String id;
+    //Rider that created request and driver that will fulfill it
+    private Rider rider;
+    private Driver driver;
+    //Location rider wishes to be picked up and dropped off at
+    private Location startLocation;
+    private Location endLocation;
+    // How much the ride will cost. Calculated based on startLocation and endLocation
+    private double fee;
+    // State of the request. See RequestEnum.java for details.
     private RequestEnum state;
+
+    public Request(Rider rider, Location start, Location end){
+        this.rider = rider;
+        this.startLocation = start;
+        this.endLocation = end;
+        this.state = RequestEnum.openRequest;
+        this.fee = calculateFee(start, end);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Rider getRider() {
+        return rider;
+    }
+
+    public void setRider(Rider rider) {
+        this.rider = rider;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public Location getStartLocation() {
+        return startLocation;
+    }
+
+    public void setStartLocation(Location startLocation) {
+        this.startLocation = startLocation;
+    }
+
+    public Location getEndLocation() {
+        return endLocation;
+    }
+
+    public void setEndLocation(Location endLocation) {
+        this.endLocation = endLocation;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    //TODO, figure out the formula for this
+    public double calculateFee(Location start, Location end){
+        return 0;
+    }
+
+    public RequestEnum getState() {
+        return state;
+    }
+
+    public void setState(RequestEnum state) {
+        this.state = state;
+    }
+
 }
