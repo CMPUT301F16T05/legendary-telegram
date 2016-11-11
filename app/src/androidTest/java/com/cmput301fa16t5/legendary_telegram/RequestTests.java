25de61e2ac52d2b3b175b7db48e97322a4a4924b
Fatechosen <idreamyc@gmail.com>
2016-11-11 16:58:54 -0700

    Request Test Code

--- app/src/androidTest/java/com/cmput301fa16t5/legendary_telegram/RequestTests.java
+++ app/src/androidTest/java/com/cmput301fa16t5/legendary_telegram/RequestTests.java
@@ -1,10 +1,11 @@
 package com.cmput301fa16t5.legendary_telegram;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import java.util.ArrayList;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertTrue;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by idrea on 2016/11/5.
@@ -12,4 +13,60 @@ import static junit.framework.Assert.assertTrue;
 
 public class RequestTests {
     //Need to be implemented
+    @Test
+    public void testAddADriver(){
+        IdentificationCard me = new IdentificationCard("a", "a@test.com", "1234567890");
+        IdentificationCard driver = new IdentificationCard("aa", "aa@test.com", "11234567890");
+        LatLng start = new LatLng(0, 0);
+        LatLng end = new LatLng(1,1);
+        Request request = new Request(me, start, end);
+
+        request.addADriver(driver);
+
+        assertTrue(request.getPotentialDrivers().contains(driver));
+        assertEquals(RequestEnum.hasADriver, request.getState());
+        assertFalse(request.isOnServer());
+    }
+
+    @Test
+    public void testAcceptADriver(){
+        IdentificationCard me = new IdentificationCard("a", "a@test.com", "1234567890");
+        IdentificationCard driver = new IdentificationCard("aa", "aa@test.com", "11234567890");
+        LatLng start = new LatLng(0, 0);
+        LatLng end = new LatLng(1,1);
+        Request request = new Request(me, start, end);
+        request.addADriver(driver);
+
+        request.acceptADriver(0);
+
+        assertEquals(driver, request.getPotentialDrivers().get(0));
+        assertEquals(RequestEnum.acceptedADriver, request.getState());
+        assertFalse(request.isOnServer());
+    }
+
+    @Test
+    public void testCheckCommittedDriver(){
+        IdentificationCard me = new IdentificationCard("a", "a@test.com", "1234567890");
+        IdentificationCard driver = new IdentificationCard("aa", "aa@test.com", "11234567890");
+        LatLng start = new LatLng(0, 0);
+        LatLng end = new LatLng(1,1);
+        Request request = new Request(me, start, end);
+
+        assertFalse(request.checkCommittedDriver(driver));
+
+        // Test for the iIsThisMe is the same as what in the Driver Test
+    }
+
+    @Test
+    public void testCommitToRequest(){
+        IdentificationCard me = new IdentificationCard("a", "a@test.com", "1234567890");
+        LatLng start = new LatLng(0, 0);
+        LatLng end = new LatLng(1,1);
+        Request request = new Request(me, start, end);
+
+        request.commitToRequest();
+
+        assertEquals(RequestEnum.driverHasCommitted, request.getState());
+    }
+
 }
