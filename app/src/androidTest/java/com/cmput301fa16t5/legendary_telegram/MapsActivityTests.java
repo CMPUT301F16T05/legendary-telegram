79a428b5548cec7f46a49e45251922e000d6a4b4
Tom <zhimao@ualberta.ca>
2016-11-10 17:20:14 -0700

    Create test for the MapsActivity using solo

---
+++ app/src/androidTest/java/com/cmput301fa16t5/legendary_telegram/MapsActivityTests.java
@@ -0,0 +1,21 @@
+package com.cmput301fa16t5.legendary_telegram;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+/**
+ * MapsActivityTests class contains unit tests for MapsActivity.
+ * @author zhimao
+ */
+
+public class MapsActivityTests extends ActivityInstrumentationTestCase2<MapsActivity> {
+    private Solo solo;
+
+    // Constructor
+    public MapsActivityTests() {
+        super(com.cmput301fa16t5.legendary_telegram.MapsActivity.class);
+    }
+
+
+}
