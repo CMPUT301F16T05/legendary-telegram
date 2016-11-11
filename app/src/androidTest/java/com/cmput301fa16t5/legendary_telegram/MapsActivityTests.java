ec850ab22c55cb468c08faf3758a443f5213a272
Tom <zhimao@ualberta.ca>
2016-11-10 17:44:14 -0700

    Fix the test code using Lab 6

--- app/src/androidTest/java/com/cmput301fa16t5/legendary_telegram/MapsActivityTests.java
+++ app/src/androidTest/java/com/cmput301fa16t5/legendary_telegram/MapsActivityTests.java
@@ -1,6 +1,8 @@
 package com.cmput301fa16t5.legendary_telegram;
 
+import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 
 import com.robotium.solo.Solo;
 
@@ -9,6 +11,7 @@ import com.robotium.solo.Solo;
  * @author zhimao
  */
 
+// Use the code from Lab 6
 public class MapsActivityTests extends ActivityInstrumentationTestCase2<MapsActivity> {
     private Solo solo;
 
@@ -17,5 +20,26 @@ public class MapsActivityTests extends ActivityInstrumentationTestCase2<MapsActi
         super(com.cmput301fa16t5.legendary_telegram.MapsActivity.class);
     }
 
+    // Start the activity
+    public void testStart() throws Exception {
+        Activity activity = getActivity();
+
+    }
+    public void setUp() throws Exception {
+        Log.d("TAG1", "setUp()");
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    // Test for Ok button
+    public void okButtonTest() {
+        solo.clickOnButton("Ok");
+
+    }
+
+    // Activity finish
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
 
 }
