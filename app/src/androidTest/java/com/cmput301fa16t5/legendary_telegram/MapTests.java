a2647caeccdc77c3fda2800a7951b633eaa3d614
Tom <zhimao@ualberta.ca>
2016-11-10 17:10:30 -0700

    Add MapTests class

---
+++ app/src/androidTest/java/com/cmput301fa16t5/legendary_telegram/MapTests.java
@@ -0,0 +1,17 @@
+package com.cmput301fa16t5.legendary_telegram;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * MapTests class contains unit tests for map.
+ * @author zhimao
+ */
+
+public class MapTests {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
