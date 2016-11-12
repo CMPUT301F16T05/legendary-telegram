8ab8683ce57fd3c19a707852962d95e03a76ef53
Tom <zhimao@ualberta.ca>
2016-11-12 13:53:22 -0700

    Send position pair to central controller

--- app/src/main/java/com/cmput301fa16t5/legendary_telegram/MapController.java
+++ app/src/main/java/com/cmput301fa16t5/legendary_telegram/MapController.java
@@ -1,5 +1,9 @@
 package com.cmput301fa16t5.legendary_telegram;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
 /**
  * MapController class is the controller of the map.
  * @author zhimao
@@ -11,6 +15,15 @@ public class MapController {
 
     public MapController() {
         centralCommand = CentralController.getInstance();
+    }
+
+    public void sendCoordinates(ArrayList<LatLng> positionPair) {
+        if (positionPair.size() == 1) {
+            centralCommand.searchRequests(positionPair);
+        } else {
+            centralCommand.createRequest(positionPair);
+        }
+
     }
 
 
