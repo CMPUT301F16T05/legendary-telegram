9f7cfb7b8df882b08f75dee04b32ae2eb54c88b4
yutang <yutang@ualberta.ca>
2016-11-19 15:53:09 -0700

    create filter activity and xml not yet doing anything yet

---
+++ app/src/main/java/com/cmput301fa16t5/legendary_telegram/FilterActivity.java
@@ -0,0 +1,13 @@
+package com.cmput301fa16t5.legendary_telegram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+    }
+}
